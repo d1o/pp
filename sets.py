bf5bc48981a3439d0643129d8f1902395170fae3
d1o <dominiklis111@gmail.com>
2018-03-17 14:05:54 +0100

    settings

---
+++ sets.py
@@ -0,0 +1,13 @@
+import pygame, sys, random
+from pygame.locals import *
+
+WIDTH = 720
+HEIGHT = 480
+title = "game"
+FPS = 40
+
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE = (  0,   0, 255)
